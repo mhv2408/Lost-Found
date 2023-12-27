3c018800770f65d9c09ed0f1f5daaddc2aa62010
Harsha Vardhan Mirthinti <mhv2408@gmail.com>
2023-12-28 02:41:58 +0530

    Updated database links

--- django_project/settings.py
+++ django_project/settings.py
@@ -17,6 +17,8 @@ from dotenv import load_dotenv
 
 load_dotenv()  
 
+print(os.environ.get('DATABASE_URL'))
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 

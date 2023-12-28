c702434be6aaaa2beeb83a097805eb2e5eaec56b
Harsha Vardhan Mirthinti <mhv2408@gmail.com>
2023-12-28 23:44:46 +0530

    Environment variables

--- django_project/settings.py
+++ django_project/settings.py
@@ -27,12 +27,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@vwgb=7qgif@0=e0i38(o&ss_p*06&6a+0ca*6t^03o#jicqh3'
+#SECRET_KEY = 'django-insecure-@vwgb=7qgif@0=e0i38(o&ss_p*06&6a+0ca*6t^03o#jicqh3'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ['lostnfound-qp3t.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split("")
 
 
 # Application definition
